6be33f8330f365d1b96f2789db015a40dd581edc
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-21 15:00:53 -0300

    refactor(dddcost-repository): Add error if result no have data

--- backend/repository/dddcost-repository/getbyorigindestination-dddcost-repository.go
+++ backend/repository/dddcost-repository/getbyorigindestination-dddcost-repository.go
@@ -9,8 +9,12 @@ import (
 // GetByOriginDestination gets the cost of a call by origin and destination
 func (r *dDDCostRepository) GetByOriginDestination(ctx context.Context, origin, destination *int) (*entity.DDDCost, *customerror.CustomError) {
 	var dddcost entity.DDDCost
-	err := r.sqlx.GetContext(ctx, &dddcost, "SELECT origin, destination, cost FROM dddcost WHERE origin = ? AND destination = ?", origin, destination)
+
+	err := r.sqlx.GetContext(ctx, &dddcost, "SELECT origin, destination, cost FROM dddcost WHERE origin = $1 AND destination = $2", *origin, *destination)
 	if err != nil {
+		if err.Error() == "sql: no rows in result set" {
+			return nil, customerror.NewError(customerror.ENOTFOUND, "Not found", "dddcost_repository.GetByOriginDestination", err)
+		}
 		return nil, customerror.NewError(customerror.EINVALID, "Internal error", "dddcost_repository.GetByOriginDestination", err)
 	}
 	return &dddcost, nil
