6218da5126f76bfcc3170f34395d6da9cde0e0f2
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-23 10:00:14 -0300

    feat(shared components): Add shared components

---
+++ frontend/src/components/home/InputNumber.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <label
+      >{{ label }}
+      <input v-model="value" type="number" @input="handleChange" />
+    </label>
+  </div>
+</template>
+<script>
+export default {
+  name: "InputNumber",
+  props: {
+    label: String,
+  },
+  data() {
+    return {
+      value: 0,
+    };
+  },
+  methods: {
+    handleChange() {
+      console.log(this.value);
+      this.$emit("updateValue", this.value);
+    },
+  },
+};
+</script>
---
+++ frontend/src/components/shared/Btn.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button type="button" @click="$emit('clickBtn')">
+      {{ labelBtn }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BtnCalc",
+  props: {
+    labelBtn: String,
+  },
+};
+</script>
