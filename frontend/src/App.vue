bc6731f684aa4d5f7442243a1a2492a7e87ce924
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-09-23 10:08:37 -0300

    feat(style): Add style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,27 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
   </nav>
   <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 nav {
-  padding: 30px;
+  display: flex;
+  flex-direction: row;
+  height: 5%;
+  justify-content: center;
 
   a {
+    color: black;
     font-weight: bold;
-    color: #2c3e50;
+    margin: 5px;
+    padding: 10px;
 
     &.router-link-exact-active {
-      color: #42b983;
+      border: 1px solid rgb(8, 8, 209);
+      border-radius: 10px;
+      color: rgb(8, 8, 209);
     }
   }
 }
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,36 @@
+<style lang="scss" scopped>
+@import "./styles/HomeView";
+</style>
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <div class="home">
+      <div class="top-home">
+        <img alt="Gopher Image" src="../assets/govue.webp" />
+        <h1>FaleMais Telzir</h1>
+      </div>
+    </div>
+    <FiltersField />
+    <TablePlansCost :show="showTable" />
+    <LoadingComponent :show="isLoading" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import FiltersField from "@/components/home/FiltersField.vue";
+import TablePlansCost from "@/components/home/TablePlansCost.vue";
+import { mapState } from "vuex";
+import LoadingComponent from "@/components/shared/LoadingComponent.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    LoadingComponent,
+    FiltersField,
+    TablePlansCost,
+  },
+  computed: {
+    ...mapState(["isLoading", "showTable"]),
   },
 });
 </script>
